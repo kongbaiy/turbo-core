1c5392cb3701abb38825427af79ecc1550e2ec40
kebi <kanglu-email@qq.com>
2026-05-20 14:32:27 +0800

    feat: vscode 新增配置

--- packages/config/stylelint/src/index.ts
+++ packages/config/stylelint/src/index.ts
@@ -2,16 +2,15 @@ export function defineConfig() {
     return {
         extends: [
             'stylelint-config-standard',
-            'stylelint-config-recommended-scss',
+            // 'stylelint-config-recommended-scss',
             // 支持 CSS Modules 的约定规则
             'stylelint-config-css-modules',
-            // 不使用 stylelint-config-prettier（与 stylelint@17 存在兼容性问题），
-            // 建议使用 Prettier 单独格式化并让 Stylelint 负责风格校验。
+            // 不使用 stylelint-config-prettier（与 stylelint@17 存在兼容性问题），建议使用 Prettier 单独格式化并让 Stylelint 负责风格校验
         ],
         plugins: [
             'stylelint-order', // 属性排序
             'stylelint-plugin-defensive-css', // 防御式 CSS 检查
-            'stylelint-scss', // SCSS 规则支持
+            // 'stylelint-scss', // SCSS 规则支持
         ],
         rules: {
             // 禁止在同一个选择器中重复声明同一属性
@@ -30,6 +29,63 @@ export function defineConfig() {
                     message:
                         'Expected class selector to be kebab-case、kebab__case',
                 },
+            ],
+            'order/order': [
+                'custom-properties',
+                'dollar-variables',
+                {
+                    type: 'at-rule',
+                    name: 'include',
+                },
+                'declarations',
+                {
+                    type: 'rule',
+                    selector: '^&',
+                },
+                'rules',
+            ],
+            'order/properties-order': [
+                'display',
+                'position',
+                'top',
+                'right',
+                'bottom',
+                'left',
+                'z-index',
+                'box-sizing',
+                'width',
+                'height',
+                'min-width',
+                'min-height',
+                'max-width',
+                'max-height',
+                'margin',
+                'margin-top',
+                'margin-right',
+                'margin-bottom',
+                'margin-left',
+                'padding',
+                'padding-top',
+                'padding-right',
+                'padding-bottom',
+                'padding-left',
+                'border',
+                'border-style',
+                'border-width',
+                'border-color',
+                'border-radius',
+                'background',
+                'background-color',
+                'background-image',
+                'background-repeat',
+                'font',
+                'font-family',
+                'font-size',
+                'font-weight',
+                'font-style',
+                'text-align',
+                'color',
+                'content',
             ],
         },
         overrides: [

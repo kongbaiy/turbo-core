712d9b60b89074f17c02feb08497a706f6be5ce9
kebi <kanglu-email@qq.com>
2026-05-20 09:29:55 +0800

    feat: apps demo 删除

---
+++ git-clone.sh
@@ -0,0 +1,7 @@
+#
+# SSH 方式克隆代码速度贼慢，改成 HTTPS 方式克隆
+# 建议全局设置 git 储存密码，只需要输入一次，避免每次输入密码
+# git config --global credential.helper store
+#
+
+git clone https://github.com/kongbaiy/turbo-core-template.git apps/turbo-core-template

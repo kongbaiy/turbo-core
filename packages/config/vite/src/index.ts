9266fa6d739c8a75b8453de57b156db81a4f5256
kebi <kanglu-email@qq.com>
2026-05-21 10:27:35 +0800

    feat: 更新

--- packages/config/vite/src/index.ts
+++ packages/config/vite/src/index.ts
@@ -1,10 +1,24 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-    plugins: [react(), UnoCSS()],
+    plugins: [
+        react(),
+        AutoImport({
+            imports: ['react'],
+            dts: './auto-imports.d.ts',
+            dirs: [path.resolve(__dirname, '../../../react-components')],
+            eslintrc: {
+                enabled: true,
+                filepath: './.eslintrc-auto-import.json',
+                globalsPropValue: true,
+            },
+        }),
+        UnoCSS(),
+    ],
     css: {
         modules: {
             // 将 kebab-case 转换为 camelCase

f0861f7fb15de5d0715023b9a7b108c007c2eebd
kebi <kanglu-email@qq.com>
2026-05-21 10:39:12 +0800

    feat: 修复eslint 忽略属性

--- packages/config/eslint/src/base.ts
+++ packages/config/eslint/src/base.ts
@@ -3,8 +3,34 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import turboPlugin from 'eslint-plugin-turbo'
 import tseslint from 'typescript-eslint'
 import onlyWarn from 'eslint-plugin-only-warn'
+import { globalIgnores } from 'eslint/config'
 
 export const baseConfig = [
+    // 全局忽略放在最前面
+    globalIgnores([
+        'dist/**',
+        'node_modules/**',
+        'build/**',
+        'coverage/**',
+        'out/**',
+        'public/**',
+        '.next/**',
+        'next-env.d.ts',
+        '**/*.d.ts',
+        '**/*.mdx',
+        '**/*.md',
+        '**/*.json',
+        '**/*.yml',
+        '**/*.yaml',
+        '**/*.css',
+        '**/*.scss',
+        '**/*.sass',
+        '**/*.less',
+        '.prettierrc.json',
+        '.stylelintrc.json',
+        '.eslintrc.json',
+        '.eslintignore',
+    ]),
     js.configs.recommended,
     eslintConfigPrettier,
     ...tseslint.configs.recommended,
@@ -20,8 +46,5 @@ export const baseConfig = [
         plugins: {
             onlyWarn,
         },
-    },
-    {
-        ignores: ['dist/**'],
     },
 ]

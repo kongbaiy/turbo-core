edba7260691cdaba0b65ad42e31c64d94160a5d8
kebi <kanglu-email@qq.com>
2026-05-21 16:30:10 +0800

    feat: 新增qiankun配置

--- packages/config/vite/src/index.ts
+++ packages/config/vite/src/index.ts
@@ -1,28 +1,57 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import qiankun from 'vite-plugin-qiankun'
 
-export default defineConfig({
-    plugins: [
-        react(),
-        AutoImport({
-            imports: ['react'],
-            dts: './auto-imports.d.ts',
-            dirs: [path.resolve(__dirname, '../../../react-components')],
-            eslintrc: {
-                enabled: true,
-                filepath: './.eslintrc-auto-import.json',
-                globalsPropValue: true,
+interface ExtendOptions {
+    plugins?: PluginOption[]
+    qiankun?: (...args: unknown[]) => unknown
+    server?: Record<string, unknown>
+}
+
+function defineAppConfig(extendOptions?: ExtendOptions) {
+    const {
+        qiankun: setQiankun,
+        plugins = [],
+        server = {}
+    } = extendOptions || {}
+    const mountQiankunApp = typeof setQiankun === 'function' ? setQiankun(qiankun) : ''
+
+    return defineConfig({
+        plugins: [
+            react(),
+
+            AutoImport({
+                imports: ['react'],
+                dts: './auto-imports.d.ts',
+                dirs: [path.resolve(__dirname, '../../../react-components')],
+                eslintrc: {
+                    enabled: true,
+                    filepath: './.eslintrc-auto-import.json',
+                    globalsPropValue: true,
+                },
+            }),
+
+            UnoCSS(),
+
+            mountQiankunApp,
+
+            ...(plugins || [])
+        ],
+        css: {
+            modules: {
+                // 将 kebab-case 转换为 camelCase
+                localsConvention: 'camelCase',
             },
-        }),
-        UnoCSS(),
-    ],
-    css: {
-        modules: {
-            // 将 kebab-case 转换为 camelCase
-            localsConvention: 'camelCase',
         },
-    },
-})
+
+        server: {
+            host: '0.0.0.0',
+            ...server
+        }
+    })
+}
+
+export default defineAppConfig

3e1d1761c982c4b56a571720dd90ffe4502c559b
kebi <kanglu-email@qq.com>
2026-09-17 18:15:09 +0800

    feat: 新增clone方式

--- git-clone.sh
+++
@@ -1,9 +0,0 @@
-#
-# SSH 方式克隆代码速度贼慢，改成 HTTPS 方式克隆
-# 建议全局设置 git 储存密码，只需要输入一次，避免每次输入密码
-# git config --global credential.helper store
-#
-
-git clone -b tabs-cache https://github.com/kongbaiy/turbo-qiankun-main.git apps/turbo-qiankun-main
-git clone -b tabs-cache https://github.com/kongbaiy/turbo-qiankun-subapp.git apps/turbo-qiankun-subapp
-git clone -b tabs-cache https://github.com/kongbaiy/turbo-qiankun-subapp2.git apps/turbo-qiankun-subapp2
